5ce736d4a69f0d520b7a407b034812b98aa7670c
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2025-11-27 22:05:33 -0800

    BFS

---
+++ Grokking Algorithms/GraphEx.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+graph = dict()
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+
+def searchThisGraph():
+
+    search_queue = deque()
+    search_queue += graph["you"]
+
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+print(searchThisGraph())
+
+# time complexity O(V + E) this means it runs for the number of people  plus the number of edges
